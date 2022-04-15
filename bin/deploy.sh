f3a5771d61a179fb8fd9ac1dc530e5baa0f793f6
Nilesh Grasia <nilesh.grasia@outlook.com>
2022-04-14 19:29:04 -0500

    [Story-0001] Added initial files

---
+++ bin/deploy.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+DIRNAME= "./bin"
+S3_BUCKET = ""
+TEMPLATE_PATH="${DIRNAME}/template.yaml"
+BUNDLE_PATH="${DIRNAME}/output/packaged-template.yaml"
+CODE_S3_BUCKET="codepipeline-us-east-2-118905992744"
+CODE_S3_BUCKET_KEY='/shared/my_code'
+DEPLOY_LOCATION="s3://$CODE_S3_BUCKET$CODE_S3_BUCKET_KEY"
+
+fucntion code_zip {
+  echo "copying folder to env "
+
+  cp my-sam-app/ my_sam_app/
+
+  echo -e "Executing zip func"
+
+  ZIP_FILE_NAME = 'hello_world_01.zip'
+  zip -r $ZIP_FILE_NAME bin/ my_sam_app/ hello_world/
+  echo -e "Uploading the zip file to S3"
+  aws s3 cp $ZIP_FILE_NAME $DEPLOY_LOCATION/$ZIP_FILE_NAME
+
+function cfn_package_build {
+  echo "Start cfn package"
+
+  mkdir ${DIRNAME}/output
+  aws aws cloudformation package --template-file ${TEMPLATE_PATH} --s3-bucket ${CODE_S3_BUCKET} --output-template-file ${BUNDLE_PATH} --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND
+  echo "Completed building Template"
+}
+
+function main{
+    cfn_package_build
+    code_zip
+
+}
+}
+main
+echo "Completed build stage"
